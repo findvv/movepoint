cfe8ad388c798fac2f150afbbd4626354af59ea3
findvv <zzxzzx2015@126.com>
2016-07-07 13:58:06 +0800

    发布新版本

--- README.md
+++ README.md
@@ -12,9 +12,9 @@ $ npm test
 ## 使用方法
 ```javascript
 var movepoint = require('sm-movepoint');
-movepoint(1.1, 2)    //  110
-movepoint(1.1, -2)    //  0.011
-movepoint(1.1, 0)    //  1.1
-movepoint(-1.1, 2)    //  -110
-movepoint(-1.1, -2)    //  -0.011
+movepoint(1.1, 2)    //  '110'
+movepoint(1.1, -2)    //  '0.011'
+movepoint(1.1, 0)    //  '1.1'
+movepoint(-1.1, 2)    //  '-110'
+movepoint(-1.1, -2)    //  '-0.011'
 ```
--- test/test.js
+++ test/test.js
@@ -3,27 +3,27 @@ var assert = require("assert")
 describe('移动小数点', function(){
   describe('movepoint(1.1, 2)', function(){
     it('小数点左移', function(){
-      assert.equal(-0.011, movepoint(-1.1, -2));
+      assert.equal('-0.011', movepoint(-1.1, -2));
     })
   })
   describe('movepoint(1.1, -2)', function(){
     it('小数点左移', function(){
-      assert.equal(0.011, movepoint(1.1, -2));
+      assert.equal('0.011', movepoint(1.1, -2));
     })
   })
   describe('movepoint(1.1, 0)', function(){
     it('数字不变', function(){
-      assert.equal(1.1, movepoint(1.1, 0) );
+      assert.equal('1.1', movepoint(1.1, 0) );
     })
   })
   describe('movepoint(-1.1, 2)', function(){
     it('补充0', function(){
-      assert.equal(110, movepoint(1.1, 2));
+      assert.equal('110', movepoint(1.1, 2));
     })
   })
   describe('movepoint(-1.1, 2)', function(){
     it('补充0', function(){
-      assert.equal(-110, movepoint(-1.1, 2));
+      assert.equal('-110', movepoint(-1.1, 2));
     })
   })
 })
